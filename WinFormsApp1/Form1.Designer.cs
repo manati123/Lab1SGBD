673cb59fa1c1f3863add1a5c2b19c24187b16a8f
Silviu <piie2806@scs.ubbcluj.ro>
2021-04-07 23:37:46 +0300

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parentGridView = new System.Windows.Forms.DataGridView();
+            this.childGridView = new System.Windows.Forms.DataGridView();
+            this.updateBD = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.parentGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // parentGridView
+            // 
+            this.parentGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.parentGridView.Location = new System.Drawing.Point(32, 12);
+            this.parentGridView.Name = "parentGridView";
+            this.parentGridView.RowTemplate.Height = 25;
+            this.parentGridView.Size = new System.Drawing.Size(673, 197);
+            this.parentGridView.TabIndex = 0;
+            // 
+            // childGridView
+            // 
+            this.childGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.childGridView.Location = new System.Drawing.Point(32, 233);
+            this.childGridView.Name = "childGridView";
+            this.childGridView.RowTemplate.Height = 25;
+            this.childGridView.Size = new System.Drawing.Size(673, 189);
+            this.childGridView.TabIndex = 1;
+            // 
+            // updateBD
+            // 
+            this.updateBD.Location = new System.Drawing.Point(746, 153);
+            this.updateBD.Name = "updateBD";
+            this.updateBD.Size = new System.Drawing.Size(173, 56);
+            this.updateBD.TabIndex = 2;
+            this.updateBD.Text = "UpdateDB";
+            this.updateBD.UseVisualStyleBackColor = true;
+            this.updateBD.Click += new System.EventHandler(this.updateBD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 450);
+            this.Controls.Add(this.updateBD);
+            this.Controls.Add(this.childGridView);
+            this.Controls.Add(this.parentGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.parentGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView parentGridView;
+        private System.Windows.Forms.DataGridView childGridView;
+        private System.Windows.Forms.Button updateBD;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly string PARENT_TABLE = "Certification";
+        private readonly string CHILD_TABLE = "Freelancer";
+        private readonly string FK = "FK_Freelancer_Certification";
+
+        private DataSet dataSet = new DataSet();
+        private SqlConnection dbConnection;
+
+        private SqlDataAdapter dataAdapterParent, dataAdapterChild;
+        private BindingSource bindingChild = new BindingSource();
+        private BindingSource bindingParent = new BindingSource();
+
+        private void InitializeDatabase()
+        {
+            this.dbConnection = new SqlConnection("Data Source=DESKTOP-0UHOTTS\\SQLEXPRESS;"
+               + "Initial Catalog = Final_Lab_Doamne_Ajuta;"
+               + "Integrated Security = True;");
+
+            this.dataAdapterChild = new SqlDataAdapter($"SELECT * FROM  {this.CHILD_TABLE}",this.dbConnection);
+            this.dataAdapterParent = new SqlDataAdapter($"SELECT * FROM {this.PARENT_TABLE}", this.dbConnection);
+
+            new SqlCommandBuilder(dataAdapterChild);
+
+            this.dataAdapterChild.Fill(this.dataSet, this.CHILD_TABLE);
+            this.dataAdapterParent.Fill(this.dataSet, this.PARENT_TABLE);
+
+            var dataRelation = new DataRelation(
+                this.FK,
+                this.dataSet.Tables[this.CHILD_TABLE].Columns["freelancer_id"],
+                this.dataSet.Tables[this.PARENT_TABLE].Columns["freelancer_id"],
+                false
+                );
+            this.dataSet.Relations.Add(dataRelation);
+        }
+
+        private void InitializeUI()
+        {
+            this.bindingChild.DataSource = this.dataSet;
+            this.bindingChild.DataMember = this.CHILD_TABLE;
+
+            this.bindingParent.DataSource = this.bindingChild;
+            this.bindingParent.DataMember = this.FK;
+
+            this.childGridView.DataSource = this.bindingChild;
+            this.parentGridView.DataSource = this.bindingParent;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void updateBD_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder cmd = new SqlCommandBuilder(this.dataAdapterParent);
+            cmd.GetInsertCommand();
+            this.dataAdapterChild.Update(dataSet, CHILD_TABLE);
+            this.dataAdapterParent.Update(dataSet, PARENT_TABLE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.InitializeDatabase();
+            this.InitializeUI();
+        }
+    }
+}
